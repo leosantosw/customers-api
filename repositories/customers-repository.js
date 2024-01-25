9526bce5a9b5306e07ce3737a72e144f82afa6b9
leosantosw <leonardosantosx0077@gmail.com>
2024-01-25 12:32:39 -0300

    feat: add coordinates in customer repository

--- repositories/customers-repository.js
+++ repositories/customers-repository.js
@@ -9,27 +9,46 @@ export class CustomerRepository {
     return results
   }
 
-  async create (user) {
-    const query = {
+  async create (customer) {
+    const customerQuery = {
       text: 'INSERT INTO customers(name, email, telephone) VALUES($1, $2, $3) RETURNING *',
-      values: [user.name, user.email, user.telephone],
+      values: [customer.name, customer.email, customer.telephone],
     }
-    console.log(query)
-    const { rows: [result] } = await client.query(query)
-    return result
+    const { rows: [createdCustomer] } = await client.query(customerQuery)
+
+    const cooderinateQuery = {
+      text: 'INSERT INTO customers_coordinates(customer_id, coordinate_x, coordinate_y) VALUES($1, $2, $3) RETURNING coordinate_x, coordinate_y',
+      values: [createdCustomer.id, customer.coordinateX, customer.coordinateY],
+    }
+
+    const { rows: [createdCustomerCoordinateds] } = await client.query(cooderinateQuery)
+
+    return {
+      ...createdCustomer,
+      ...createdCustomerCoordinateds,
+    }
   }
 
   buildListQuery (filters) {
     const filterClauses = Object.entries(filters)
-      .filter(([key, value]) => value !== undefined && value !== null)
-      .map(([key, value], index) => `LOWER(${key}) LIKE LOWER($${index + 1})`)
+      .filter(([_, value]) => value !== undefined && value !== null)
+      .map(([key, _], index) => `LOWER(${key}) LIKE LOWER($${index + 1})`)
       .join(' AND ')
 
     const values = Object.values(filters)
       .filter(value => value !== undefined && value !== null)
       .map(value => `%${value}%`)
 
-    const query = `SELECT * FROM customers${filterClauses ? ' WHERE ' + filterClauses : ''}`
+    const query = `SELECT
+      customers.id,
+      customers.name,
+      customers.email,
+      customers.telephone,
+      customers_coordinates.coordinate_x,
+      customers_coordinates.coordinate_y
+    FROM customers
+    INNER JOIN customers_coordinates ON customers.id = customers_coordinates.customer_id
+    ${filterClauses ? ' WHERE ' + filterClauses : ''}`
 
     return { query, values }
   }
