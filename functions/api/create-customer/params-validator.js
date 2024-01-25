60fefc845bf17c31fa27741c96a65500290936d5
leosantosw <leonardosantosx0077@gmail.com>
2024-01-25 13:48:43 -0300

    refactor: remove cosole log

--- functions/api/create-customer/params-validator.js
+++ functions/api/create-customer/params-validator.js
@@ -1,7 +1,6 @@
 export const paramsValidator = params => {
   const requiredParams = ['name', 'email', 'telephone', 'coordinate_x', 'coordinate_y']
   for (const requiredParam of requiredParams) {
-    console.log(params[requiredParam])
     if (!params[requiredParam]) {
       return {
         error: 'missing_param',
