79143063cad3ff414d93247810179c40a70874c3
leosantosw <leonardosantosx0077@gmail.com>
2024-01-25 12:31:36 -0300

    alter database structure

--- database/migrations/create_customers_table.sql
+++ database/migrations/create_customers_table.sql
@@ -1,6 +1,16 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE IF NOT EXISTS customers (
-  id SERIAL PRIMARY KEY,
+  id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
   name VARCHAR(255),
   email VARCHAR(255),
   telephone VARCHAR(20)
-)
+);
+
+CREATE TABLE IF NOT EXISTS customers_coordinates (
+  id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+  customer_id UUID,
+  FOREIGN KEY (customer_id) REFERENCES customers(id),
+  coordinate_x FLOAT,
+  coordinate_y FLOAT
+);
